d3a20d9880975e32922deae0a5e72a8bf31544fe
PulserCoder <pashenka.kuzmin.2006@gmail.com>
2022-01-16 11:12:04 +0300

    Передвижение главного корабля

--- main.py
+++ main.py
@@ -5,26 +5,42 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+x_pos = 400
+v = 5
+fps = 90
+
+
+def check_gran(x, v):
+    if x >= 800:
+        return -v
+    if x <= 0:
+        return -v
+    return v
 
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 800
     screen = pygame.display.set_mode(size)
-    running = True
-    x_pos = 0
     clock = pygame.time.Clock()
-    v = 200
-    fps = 90
+    running = True
     while running:
         screen.fill(GREEN)
-        pygame.draw.circle(screen, RED, (x_pos, 400), 20)
-        x_pos += clock.tick(fps) * v / 1000
-        if x_pos >= 800:
-            x_pos = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            x_pos -= check_gran(x_pos, v)
+        if keys[pygame.K_RIGHT]:
+            print(x_pos)
+            x_pos += check_gran(x_pos, v)
+            print(x_pos)
+            #if event.type == pygame.KEYDOWN:
+            #    if event.key == pygame.K_RIGHT:
+            #        x_pos = ship.movement_ship_right()
+            #    elif event.key == pygame.K_LEFT:
+            #        x_pos = ship.movement_ship_left()
+        pygame.draw.circle(screen, RED, (x_pos, 700), 20)
         pygame.display.flip()
+        clock.tick(fps)
     pygame.quit()
-
-
