c3ee357ea331f797e86958381699073459fee69b
AssasinFrost <sbevol05@rambler.ru>
2022-01-24 23:35:58 +0300

    BETA(0.2)

--- main.py
+++ main.py
@@ -3,8 +3,86 @@ import sys
 
 import pygame
 
+class Level:
+    def __init__(self, time, ships):
+        self.time = time
+        self.timeout = 900
+        self.ships = ships
+        self.all_sprites = pygame.sprite.Group()
+        size = width, height = 800, 800
+        self.screen = pygame.display.set_mode(size)
+        self.heart = pygame.sprite.Sprite()
+        self.heart.image = load_image("heart3.png")
+        self.heart.rect = self.heart.image.get_rect()
+        self.all_sprites.add(self.heart)
+        self.health = 3
+        self.hp = HealthPoint(self.health)
+        self.em1 = Enemy(self.screen, 3, 400, 100, speed=2)
+        enemies.append(self.em1)
+        self.ship = Ship(self.screen)
+
+    def start(self):
+        pygame.init()
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            self.screen.fill(GREEN)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                self.ship.move(left)
+            if keys[pygame.K_RIGHT]:
+                self.ship.move(right)
+            if keys[pygame.K_SPACE] and not self.timeout != 1800:
+                self.ship.shoot()
+                self.timeout = 0
+            if self.em1.t != False:
+                self.em1.render()
+            for i in bullets:
+                i.render()
+                i.move()
+                if i.coords in self.ship.hitbox and i.vector == 1 and i.isactive:
+                    self.hp.hp -= 1
+                    bullets.remove(i)
+                    i.isactive = False
+                for j in enemies:
+                    if i.coords in j.hitbox and j.t and i.vector == -1 and i.isactive:
+                        self.em1.die()
+                        i.isactive = False
+                        bullets.remove(i)
+            for i in enemies:
+                if i.t:
+                    i.cooldownupdate()
+                if i.t:
+                    i.move()
+                    i.hitboxupdate()
+                if i.pos_y > 830:
+                    i.vector = 0
+                    enemies.remove(i)
+                    self.hp.hp -= 1
+            if self.hp.hp == 3:
+                filename = 'heart3.png'
+            elif self.hp.hp == 2:
+                filename = 'heart2.png'
+            elif self.hp.hp == 1:
+                filename = 'heart.png'
+            self.heart.image = load_image(filename)
+            self.ship.render()
+            self.all_sprites.draw(self.screen)
+            self.hp.render()
+            self.heart.rect.x, self.heart.rect.y = 600, 20
+            pygame.display.flip()
+            if self.timeout != 1800:
+                self.timeout += fps
+            clock.tick(fps)
+        pygame.quit()
+
+
 class Ship:
-    def __init__(self):
+    def __init__(self, screen):
+        self.screen = screen
         self.pos_x = 400
         self.pos_y = 700
         self.coords = (self.pos_x, self.pos_y)
@@ -14,14 +92,15 @@ class Ship:
                 self.hitbox.append((i, j))
 
     def move(self, side):
-        self.pos_x += side
-        self.hitboxupdate()
+        if self.pos_x + side > 20 and self.pos_x + side < 780:
+            self.pos_x += side
+            self.hitboxupdate()
 
     def render(self):
-        pygame.draw.circle(screen, RED, (self.pos_x, self.pos_y), 20)
+        pygame.draw.circle(self.screen, RED, (self.pos_x, self.pos_y), 20)
 
     def shoot(self):
-        bullet = Bullet(screen, (self.pos_x, self.pos_y))
+        bullet = Bullet(self.screen, (self.pos_x, self.pos_y))
         bullets.append(bullet)
 
     def hitboxupdate(self):
@@ -52,12 +131,13 @@ class HealthPoint:
         self.hp = hp
 
     def render(self):
-        heart.rect.x, heart.rect.y = 600, 20
+        pass
 
 
 class Enemy:
-    def __init__(self, hp, pos_x, pos_y, speed=4, vector=1, shootrate=5400):
+    def __init__(self, screen, hp, pos_x, pos_y, speed=4, vector=1, shootrate=5400):
         self.hp = hp
+        self.screen = screen
         self.shootrate = shootrate
         self.cooldown = 0
         self.pos_x = pos_x
@@ -78,7 +158,7 @@ class Enemy:
             self.shoot()
 
     def render(self):
-        pygame.draw.circle(screen, RED, (self.pos_x, self.pos_y), 20)
+        pygame.draw.circle(self.screen, RED, (self.pos_x, self.pos_y), 20)
 
     def die(self):
         self.t = False
@@ -94,7 +174,7 @@ class Enemy:
                 self.hitbox.append((i, j))
 
     def shoot(self):
-        bullet = Bullet(screen, (self.pos_x, self.pos_y), 3, 1)
+        bullet = Bullet(self.screen, (self.pos_x, self.pos_y), 3, 1)
         bullets.append(bullet)
 
 
@@ -106,11 +186,10 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 x_pos = 400
 fps = 90
-bullets = []
-enemies = []
-timeout = 900
 left = -5
 right = 5
+bullets = []
+enemies = []
 
 
 def load_image(name, colorkey=None):
@@ -123,79 +202,5 @@ def load_image(name, colorkey=None):
     return image
 
 
-all_sprites = pygame.sprite.Group()
-heart = pygame.sprite.Sprite()
-heart.image = load_image("heart3.png")
-heart.rect = heart.image.get_rect()
-all_sprites.add(heart)
-health = 3
-hp = HealthPoint(health)
-em1 = Enemy(3, 400, 100, speed=2)
-enemies.append(em1)
-ship = Ship()
-
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 800, 800
-    screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
-    running = True
-    while running:
-        screen.fill(GREEN)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            ship.move(left)
-        if keys[pygame.K_RIGHT]:
-            ship.move(right)
-        if keys[pygame.K_SPACE] and not timeout != 1800:
-            ship.shoot()
-            timeout = 0
-        if em1.t != False:
-            em1.render()
-        for i in bullets:
-            i.render()
-            i.move()
-            if i.coords in ship.hitbox and i.vector == 1 and i.isactive:
-                hp.hp -= 1
-                bullets.remove(i)
-                i.isactive = False
-            for j in enemies:
-                if i.coords in j.hitbox and j.t and i.vector == -1 and i.isactive:
-                    em1.die()
-                    i.isactive = False
-                    bullets.remove(i)
-        for i in enemies:
-            if i.t:
-                i.cooldownupdate()
-            if i.t:
-                i.move()
-                i.hitboxupdate()
-            if i.pos_y > 830:
-                i.vector = 0
-                enemies.remove(i)
-                hp.hp = hp.hp - 1
-        if hp.hp == 3:
-            filename = 'heart3.png'
-        elif hp.hp == 2:
-            filename = 'heart2.png'
-        elif hp.hp == 1:
-            filename = 'heart.png'
-        heart.image = load_image(filename)
-        ship.render()
-        all_sprites.draw(screen)
-        hp.render()
-
-        pygame.display.flip()
-        if timeout != 1800:
-            timeout += fps
-        clock.tick(fps)
-    pygame.quit()
-
-# if event.type == pygame.KEYDOWN:
-#    if event.key == pygame.K_RIGHT:
-#        x_pos = ship.movement_ship_right()
-#    elif event.key == pygame.K_LEFT:
-#        x_pos = ship.movement_ship_left()
+game = Level(180, 50)
+game.start()
